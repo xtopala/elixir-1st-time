af54a99f25eaf3d2462b7c0005e45ca18ccf28db
xtopala <xtopala@gmail.com>
2020-09-12 13:59:35 +0200

    Finished cards project

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,13 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck makes 20 cards" do
+    deck_lenght = length(Cards.create_deck())
+    assert deck_lenght == 20
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck()
+    refute deck == Cards.save(deck)
   end
 end
