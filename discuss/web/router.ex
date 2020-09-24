ee0570013ee150b63fe67a84f8f5a1667cda21eb
xtopala <xtopala@gmail.com>
2020-09-24 17:23:03 +0200

    Starting OAuth

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -17,9 +17,12 @@ defmodule Discuss.Router do
     # Use the default browser stack
     pipe_through(:browser)
 
-    get("/", PageController, :index)
-    get("/topics/new", TopicController, :new)
-    post("topics", TopicController, :create)
+    # get("/", TopicController, :index)
+    # post("topics", TopicController, :create)
+    # get("/topics/new", TopicController, :new)
+    # get("/topics/:id/edit", TopicController, :edit)
+    # put("/topics/:id", TopicController, :update)
+    resources("/", TopicController)
   end
 
   # Other scopes may use custom stacks.
