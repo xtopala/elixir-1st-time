f7f50c558d59b48187541c0d2e7f05f51d9a25bb
xtopala <xtopala@gmail.com>
2020-09-25 12:53:54 +0200

    Finished OAuth

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -14,8 +14,7 @@ config :discuss, Discuss.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "asqDcWvSdxaJRxDQzKS1qPxbH87wU8aisFjKzypmCOyYFGWxtpcPA0shcVkflNwU",
   render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Discuss.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Discuss.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,4 +23,14 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: {Ueberauth.Strategy.Github, [default_scope: "user:email"]}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "7659e63cf1c0bd87e14e",
+  # Google: How to hide API keys
+  client_secret: "1e77cc183ad6b314ab7bc46a6b413cc4cdf1e805"
